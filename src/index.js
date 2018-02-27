c27e66f9e9c814225d671596c76a45d78914c7e0
Thomas Tuts <thomas@thomastuts.com>
2018-02-27 20:46:55 +0100

    Add additional user and exercise properties to schema

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 const typeDefs = importSchema(path.join(__dirname, 'schemas/_query.graphql'));
 
 const resolvers = {
   Query: {
-    user: (_, { name }) => ({ name: name || 'John Doe' }),
+    viewer: (_, { name }, context) => {
+      return context.user;
+    },
   },
+  User: {
+    exercises: () => [],
+  }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const authMiddleware = (req, res, next) => {
+  const authToken = req.headers.authorization ? req.headers.authorization.replace('Bearer ', '') : null;
+
+  if (authToken) {
+    // TODO: fetch user
+    req.user = { name: authToken };
+  }
+
+  next();
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: ({ request }) => {
+    return {
+      user: request.user,
+    };
+  }
+});
+
+server.express.use(bodyParser.json());
+server.express.use(authMiddleware);
+
+server.express.post('/login', (req, res) => {
+  res.json({
+    authToken: 'TODO',
+  });
+});
 
 (async () => {
   await server.start();
